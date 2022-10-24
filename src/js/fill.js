bf2a7698c114d115d54899b1fd61911bc28d95f6
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-10-24 13:53:28 +0200

    add card styles

--- src/js/fill.js
+++ src/js/fill.js
@@ -1,25 +1,25 @@
 "use strict";
 
-const arrowUp = document.querySelector(".fill-arrow-up");
-const arrowDown = document.querySelector(".fill-arrow-down");
-const formContainer = document.querySelector(".form-container");
+const arrowUpFill = document.querySelector(".fill-arrow-up");
+const arrowDownFill = document.querySelector(".fill-arrow-down");
+const formContainerFill = document.querySelector(".form-container");
 
-arrowUp.addEventListener("click", (e) => {
+arrowUpFill.addEventListener("click", (e) => {
   e.preventDefault();
-  if (arrowUp.classList.contains("hidden")) {
-    arrowUp.classList.remove("hidden");
+  if (arrowUpFill.classList.contains("hidden")) {
+    arrowUpFill.classList.remove("hidden");
   } else {
-    arrowUp.classList.add("hidden");
-    arrowDown.classList.remove("hidden");
+    arrowUpFill.classList.add("hidden");
+    arrowDownFill.classList.remove("hidden");
   }
 });
-arrowDown.addEventListener("click", (e) => {
+arrowDownFill.addEventListener("click", (e) => {
   e.preventDefault();
-  if (arrowDown.classList.contains("hidden") && paleteColors.classList.contains("hidden")) {
-    arrowDown.classList.remove("hidden");
+  if (arrowDownFill.classList.contains("hidden") && paleteColors.classList.contains("hidden")) {
+    arrowDownFill.classList.remove("hidden");
     paleteColors.classList.remove("hidden");
   } else {
-    arrowDown.classList.add("hidden");
-    arrowUp.classList.remove("hidden");
+    arrowDownFill.classList.add("hidden");
+    arrowUpFill.classList.remove("hidden");
   }
 });
