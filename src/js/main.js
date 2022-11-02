279f84ecc87650704c70db2710a6887bbf137fae
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-02 13:26:28 +0100

    Switch function to show sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,21 @@
 "use strict";
 
-// function handleOnClick(up, down, container) {
-//   up.classList.toggle("hidden");
-//   down.classList.toggle("hidden");
-//   container.classList.toggle("hidden");
-// }
-
 function handleOnClick(up, down, container) {
-  if (container.classList.contains("closed")) {
-    container.classList.remove("closed");
-    container.setAttribute("style", "height: 175px");
-  } else {
-    container.classList.add("closed");
-    container.setAttribute("style", "height: 0px");
-  }
+  up.classList.toggle("hidden");
+  down.classList.toggle("hidden");
+  container.classList.toggle("hidden");
 }
+
+// function handleOnClick(up, down, container) {
+//   const offSet = container.offsetHeight;
+//   if (container.classList.contains("closed")) {
+//     container.classList.remove("closed");
+//     container.setAttribute("style", `height: ${offSet}`);
+//   } else {
+//     container.classList.add("closed");
+//     container.setAttribute("style", `height: ${offSet}`);
+//   }
+// }
 
 arrowUpDesign.addEventListener("click", () => handleOnClick(arrowUpDesign, arrowDownDesign, formContainerDesign));
 arrowDownDesign.addEventListener("click", () => handleOnClick(arrowUpDesign, arrowDownDesign, formContainerDesign));
