2a52f6284065051c4a3cec734f87e04cfc3a0be9
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-11-02 14:00:45 +0100

    fix fill part

--- src/js/1_fill.js
+++ src/js/1_fill.js
@@ -1,48 +1,5 @@
 "use strict";
 
-// // let name = "";
-
-// // nameInput.addEventListener("input", () => {
-// //   name = nameInput.value;
-
-// //   if (name === "") {
-// //     previewName.innerHTML = "Nombre Apellidos";
-// //   } else {
-// //     previewName.innerHTML = name;
-// //   }
-// // });
-
-// function updatePreview(){
-//     console.log(nameInput);
-//     if (nameInput === "") {
-//         previewName.innerHTML = "Nombre Apellidos";
-//     }
-//     if (jobInput === "") {
-//         previewJob.innerHTML = "Front-end Developer";
-//     } else { 
-//         previewJob.innerHTML = jobInput;
-//     }
-//     if (emailInput === "") {
-//         previewEmail.href="";
-//     } else {
-//         previewEmail.href=`mailto: ${emailInput}`;
-//     }
-// };
-
-// form.addEventListener('input', (e) => {
-//     const elementName = event.target.name;
-//     const value = event.target.value;
-
-//     if (elementName === "completeName") {
-//        nameInput = value;
-//     } else if (elementName === "position"){
-//         jobInput = value;
-//     } else if (elementName === "email"){
-//         emailInput = value;
-//     }
-//     updatePreview();
-// });
-
 //Datos que cambiará el código de la función manejadora
 const data = {
     palette: '',
@@ -57,46 +14,28 @@ const data = {
 
 
 //Función que va a coger los datos de data, que están en variables, y los va a modificar
-function updatePreview() {
+function updatePreview(value) {
   if (data.name === '') {
     previewName.innerHTML = 'Nombre Apellidos';
   } else {
-    previewName.innerHTML = name;
+    previewName.innerHTML = value;
+    console.log(data.name);
   }
   if (data.job === '') {
     previewJob.innerHTML = 'Front-end developer';
   } else {
-    previewJob.innerHTML = job;
+    previewJob.innerHTML = value;
   }
   if (data.email === '') {
     previewEmail.href = '';
   } else {
-    previewEmail.href = `mailto:${email}`;
+    previewEmail.href = `mailto:${value}`;
   }
 }
 
-
-let name = '';
-let job = '';
-let email = '';
-
-
-// nameInput.addEventListener('input', () => {
-//   name = nameInput.value;
-//   updatePreview();
-// });
-
-// jobInput.addEventListener('input', () => {
-//   job = jobInput.value;
-//   updatePreview();
-// });
-// emailInput.addEventListener('input', () => {
-//   email = emailInput.value;
-//   updatePreview();
-// });
-
-
-
+// let name = '';
+// let job = '';
+// let email = '';
 
 //Listening in all form. Se le pone un evento de tipu input que empiece a funcionar cada vez que se haga click en un input. Se está escuchando todo el formulario, así que hay que saber en qué input está escribiendo y qué valor ha escrito.
 
@@ -106,29 +45,22 @@ form.addEventListener('input', (event) => {
   const value = event.target.value; //Donde está escribiendo la usuaria, todo lo que escriba hasta ahora se guarda aquí. Nosotros a su vez lo guardamos en la variable value.
   
   if(elementName === 'name') { //Si la usuaria está escribiendo en el input que tiene name: 'name';
-    data[elementName] = value; //guarda lo que está escribiendo en este input dentro de la variable value. Este value se crea solo al rellenar el input.
-
+    data.name = value; //guarda lo que está escribiendo en este input dentro de la variable value. Este value se crea solo al rellenar el input.
     //Si lo hacemos con [elementName] ya no nos hace falta esto de abajo:
-//   } else if (elementName === 'job') {
-//     data.job = value;
-//   } else if (elementName === 'email') {
-//     data.email = value;
-//   } else if (elementName === "phone") {
-//     data.phone = value;
-//   } else if (elementName === 'linkedin') {
-//     data.linkedin = value;
-//   } else if (elementName === 'github') {
-//     data.github = value;
+ } else if (elementName === 'job') {
+  data.job = value;
+ } else if (elementName === 'email') {
+   data.email = value;
+} else if (elementName === "phone") {
+  data.phone = value;
+ } else if (elementName === 'linkedin') {
+   data.linkedin = value;
+ } else if (elementName === 'github') {
+  data.github = value;
   }
   
+//   console.log(elementName);
+//   console.log(value);
   
-  
-  updatePreview();
+  updatePreview(value);
 })
-
-// data.job = 'hola';
-// data['job'] = 'adios';
-
-//Es lo mismo que:
-// const propertyName = 'job';
-// console.log(data[propertyName]);
---
+++ src/js/old.js
@@ -0,0 +1,35 @@
+// form.addEventListener('input', (e) => {
+//     const elementName = event.target.name;
+//     const value = event.target.value;
+
+//     if (elementName === "completeName") {
+//        nameInput = value;
+//     } else if (elementName === "position"){
+//         jobInput = value;
+//     } else if (elementName === "email"){
+//         emailInput = value;
+//     }
+//     updatePreview();
+// });
+
+// nameInput.addEventListener('input', () => {
+//   name = nameInput.value;
+//   updatePreview();
+// });
+
+// jobInput.addEventListener('input', () => {
+//   job = jobInput.value;
+//   updatePreview();
+// });
+// emailInput.addEventListener('input', () => {
+//   email = emailInput.value;
+//   updatePreview();
+// });
+
+
+// data.job = 'hola';
+// data['job'] = 'adios';
+
+//Es lo mismo que:
+// const propertyName = 'job';
+// console.log(data[propertyName]);
