ae9eec5442f1309ed3b3030fba0ae04fd85491c3
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-11-02 16:02:00 +0100

    update event

--- src/js/1_fill.js
+++ src/js/1_fill.js
@@ -64,22 +64,14 @@ form.addEventListener('input', (event) => {
   const elementName = event.target.name; //Este event.target está diciendo sobre qué input escribe la usuaria. Name es el atributo gancho para conectar los inputs desde html con esto.
 
   const value = event.target.value; //Donde está escribiendo la usuaria, todo lo que escriba hasta ahora se guarda aquí. Nosotros a su vez lo guardamos en la variable value.
+
+  //Si la usuaria está escribiendo en el input que tiene name: 'name':
   
-  if(elementName === 'name') { //Si la usuaria está escribiendo en el input que tiene name: 'name';
-    data.name = value; //guarda lo que está escribiendo en este input dentro de la variable value. Este value se crea solo al rellenar el input.
-    //Si lo hacemos con [elementName] ya no nos hace falta esto de abajo:
- } else if (elementName === 'job') {
-  data.job = value;
- } else if (elementName === 'email') {
-   data.email = value;
-} else if (elementName === "phone") {
-  data.phone = value;
- } else if (elementName === 'linkedin') {
-   data.linkedin = value;
- } else if (elementName === 'github') {
-  data.github = value;
-  }
-  
-  
+  if(elementName === [elementName]) { 
+    data[elementName] = value;
+  } 
+
+  //guarda lo que está escribiendo en este input dentro de la variable value. Este value se crea solo al rellenar el input.
+
   updatePreview();
-})
+});
--- src/js/old.js
+++ src/js/old.js
@@ -33,3 +33,17 @@
 //Es lo mismo que:
 // const propertyName = 'job';
 // console.log(data[propertyName]);
+
+    //Si lo hacemos con [elementName] ya no nos hace falta esto de abajo:
+
+//  } else if (elementName === 'job') {
+//   data.job = value;
+//  } else if (elementName === 'email') {
+//    data.email = value;
+// } else if (elementName === "phone") {
+//   data.phone = value;
+//  } else if (elementName === 'linkedin') {
+//    data.linkedin = value;
+//  } else if (elementName === 'github') {
+//   data.github = value;
+//   }
